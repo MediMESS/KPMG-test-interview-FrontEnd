cf0cf2687fce21e2c016a30246b1d8dc0a9e31d1
Mehdi Messarat <medimessarat@gmail.com>
2021-02-17 15:13:19 +0100

    Fixing the .env to fetch from the backend

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,7 @@
-export const API_URL =
-  process.env.NODE_ENV == "production"
-    ? process.env.SERVER_URL
-    : "http://127.0.0.1:8000";
+require("dotenv").config();
+module.exports = {
+  API_URL:
+    process.env.NODE_ENV === "production"
+      ? process.env.SERVER_URL
+      : "http://127.0.0.1:8000",
+};
