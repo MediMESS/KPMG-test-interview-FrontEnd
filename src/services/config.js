fe8270147d2dfa30ad73a92f34ccfb04605ded18
Mehdi Messarat <medimessarat@gmail.com>
2021-02-17 15:36:14 +0100

    Fixing the url not accepted in the front end

--- src/services/config.js
+++ src/services/config.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 module.exports = {
   API_URL:
     process.env.NODE_ENV === "production"
-      ? process.env.SERVER_URL
+      ? "https://kpmg-test-interview-backend.herokuapp.com"
       : "http://127.0.0.1:8000",
 };
